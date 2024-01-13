283c01a1f6dd20f0e9edd46b290257c8134e3066
Nick Faddis <nfaddis02@gmail.com>
2024-01-13 13:18:28 -0600

    Add all files

---
+++ android/app/src/main/java/com/example/reactCapacitor/MainActivity.java
@@ -0,0 +1,7 @@
+package com.example.reactCapacitor;
+
+import com.ahm.capacitor.camera.preview.CameraPreview;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
